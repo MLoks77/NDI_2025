6741178b4deeb813a6ecd53fcf0af82c03745e96
Maxime <maximederenes@gmail.com>
2025-12-05 01:44:55 +0100

    push footer

---
+++ TheHive/pages/mentionslegales/mentionslegales.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once "../../php/navbar.php";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>The Hive - Mentions légales</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<?php
+require_once "../../php/footer.php";
+?>
---
+++ TheHive/php/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <section class="bg-white py-10 flex justify-center ">
+        <div class="flex flex-row items-center gap-4">
+            <hr class="w-128 border-gray-700">
+            <img src="../../images/logonirdnoir.png" width="75" height="75">
+            <hr class="w-128 border-gray-700">
+        </div>
+    </section>
+    <section>
+        <div class="mb-5 flex flex-row justify-center">
+            <p class="bold pr-2 text-xl">@2025 Thehive</p>
+            <a class="hover:text-bold border-l pl-2 border-black text-xl" href="../../pages/mentionslegales.php">Mentions légales</a>
+        </div>
+    </section>
+</footer>
