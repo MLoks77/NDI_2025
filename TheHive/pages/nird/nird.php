0acad2cee17a63fac80966a02a79303f7baf438e
Maxime <maximederenes@gmail.com>
2025-12-05 03:51:20 +0100

    push favicon, bird, mentions

--- TheHive/pages/nird/nird.php
+++ TheHive/pages/nird/nird.php
@@ -5,30 +5,40 @@ require_once "../../php/navbar.php";
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>The Hive - NIRD</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/style.css">
+
+    <link rel="icon" type="image/png" href="../../images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="../../images/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="../../images/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="../../images/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="THEHIVE" />
+    <link rel="manifest" href="../../images/favicon/site.webmanifest" />
+    
 </head>
 
 
 
 <body>
-<div class="w-full h-screen relative">
-    <img src="../../images/NIRD_wallpaper.png" 
-         alt="Landing Background" 
-         class="object-cover w-full h-full absolute inset-0 z-50" 
-         style="width:100vw; height:100vh;">
-    <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-40 z-10">
+    <div class="w-full h-screen relative">
+        <img src="../../images/NIRD_wallpaper.png" alt="Landing Background"
+            class="object-cover w-full h-full absolute inset-0 z-50" style="width:100vw; height:100vh;">
+        <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-40 z-10">
+        </div>
+        <!-- Divider montagne en bas -->
+        <div class="custom-shape-divider-bottom-1764892962">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                preserveAspectRatio="none">
+                <path
+                    d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                    class="shape-fill"></path>
+            </svg>
+        </div>
     </div>
-    <!-- Divider montagne en bas -->
-    <div class="custom-shape-divider-bottom-1764892962">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-    </svg>
-</div>
-</div>
 </body>
 
 
--- TheHive/php/footer.php
+++ TheHive/php/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <section class="bg-white py-10 flex justify-center ">
+    <section class="bg-white py-10 flex justify-center bg-white">
         <div class="flex flex-row items-center gap-4">
             <hr class="w-128 border-gray-700">
             <img src="../../images/logonirdnoir.png" width="75" height="75">
@@ -7,9 +7,9 @@
         </div>
     </section>
     <section>
-        <div class="mb-5 flex flex-row justify-center">
-            <p class="bold pr-2 text-xl">@2025 Thehive</p>
-            <a class="hover:text-bold border-l pl-2 border-black text-xl" href="../../pages/mentionslegales/mentionslegales.php">Mentions légales</a>
+        <div class="mb-5 flex flex-row justify-center bg-white">
+            <p class="bold pr-2 text-xl ">@2025 Thehive </p>
+            <a class="hover:text-[#6B6B6B] border-l pl-2 border-black text-xl" href="../../pages/mentionslegales/mentionslegales.php"> Mentions légales</a>
         </div>
     </section>
 </footer>
