dcc0e43ede451f10c48060a1d8c0a2af46db54b4
Maxime <maximederenes@gmail.com>
2025-12-05 06:05:59 +0100

    push index fix

---
+++ TheHive/pages/piliers/piliers.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once "../../php/navbar.php";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>The Hive - les piliers</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+
+    <link rel="icon" type="image/png" href="../../images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="../../images/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="../../images/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="../../images/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="THEHIVE" />
+    <link rel="manifest" href="../../images/favicon/site.webmanifest" />
+    
+</head>
+
+<?php
+require_once "../../php/footer.php";
+?>
